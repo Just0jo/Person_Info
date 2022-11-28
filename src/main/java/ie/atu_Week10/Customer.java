124cd2c63ba3a3417352cc77602336a498e4fac2
Just0jo <G00407342@atu.ie>
2022-11-28 10:49:45 +0000

    test customer

---
+++ src/main/java/ie/atu_Week10/Customer.java
@@ -0,0 +1,33 @@
+package ie.atu_Week10;
+
+public class Customer extends Person {
+    private String CustomerNumber;
+    private boolean MailingList;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String address, String number, String customerNumber, boolean mailingList) {
+        super(name, address, number);
+        this.CustomerNumber = customerNumber;
+        this.MailingList = mailingList;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + super.toString() +
+                "CustomerNumber='" + CustomerNumber + '\'' +
+                ", MailingList=" + MailingList +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
--- src/main/java/ie/atu_Week10/PersonApp.java
+++ src/main/java/ie/atu_Week10/PersonApp.java
@@ -6,6 +6,12 @@ public class PersonApp {
         myInfo.setAddress("Glasan");
         myInfo.setName("Praise");
         myInfo.setNumber("0899");
+
         System.out.println("here are my details " + myInfo.toString());
+        //test customer
+        Customer myCustomer = new Customer("Praise","Glasan","0899","H914785",true );
+        System.out.println("Here are ym Details"+ myCustomer.toString());
+
+
     }
 }
